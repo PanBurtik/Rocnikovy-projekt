2433079f20b40c6218329c22ba83bd3eb84d6c0a
PanBurtik <panburticek@seznam.cz>
2022-12-31 16:28:53 +0100

    uprava-struktury

---
+++ mysite/object/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+def object_path(instance, filename):
+    return "object/" + str(instance.name) + "/fotka/" + filename
+
+
+class Object(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Jméno:")
+
+    object = models.CharField(max_length=100, verbose_name="Název objektu:")
+    
+    description = models.TextField(blank=True, null = True, verbose_name="Popis objektu:")
+
+    photo = models.FileField(upload_to=object_path, blank=True, null=True, verbose_name="Fotka:")
+
+    
+
+    class Meta:
+        ordering = ['name']
+        verbose_name_plural = "Objects"
+
+    def __str__(self):
+        return self.name
---
+++ mysite/object/views.py
@@ -0,0 +1,61 @@
+from multiprocessing import context
+from django.shortcuts import render, redirect
+from object.models import Object
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+# Create your views here.
+def index(request): 
+
+    num_objects = Object.objects.all().count
+
+    objects = Object.objects.order_by('name')
+
+    context ={
+        'num_objects': num_objects,
+        'objects': objects,
+    }
+    
+    return render(request, 'index.html', context = context)
+
+    
+
+def home(request):
+    return render(request, 'index.html')
+
+
+def model(request):
+    return render(request, 'model.html')
+
+
+def addObject(request):
+    if request.method == "POST":
+        prod = Object()
+        prod.name = request.POST.get('name')
+        prod.object = request.POST.get('object')
+        prod.description = request.POST.get('description')
+
+        if len(request.FILES) != 0:
+            prod.photo = request.FILES['photo']
+
+        prod.save()
+        messages.success(request, 'Objekt byl přidán do databáze')
+        return redirect('/')
+
+    return render(request, 'addObj.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+
+def login(request):
+
+    return render(request, 'login.html')
